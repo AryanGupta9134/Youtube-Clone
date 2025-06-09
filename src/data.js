008d1ec635ced645937e429c8b267626c3533253
Aryan <aryan.gupta158935@gmail.com>
2025-06-09 14:48:12 +0530

    Final Commit

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-// API Code here 
+export const API_KEY = 'AIzaSyCSPoXRmuGlWdGmVANdboDh8X6tx_A5xVA';
 export const value_converter =(value) =>{
     if(value>=1000000){
         return Math.floor(value/1000000) + "M";
